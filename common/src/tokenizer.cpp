525a84cdc7999153354f129f0c6399ded9f4a87d
Stefano Anelli <stefano.anelli@gmail.com>
2019-05-20 23:20:54 +0100

    common - Started implementation of the tokenizer

---
+++ common/src/tokenizer.cpp
@@ -0,0 +1,6 @@
+#include <gasp/tokenizer.hpp>
+#include <string>
+
+using namespace gasp::common;
+
+template class tokenizer<int>;
--- compiler/src/main.cpp
+++ compiler/src/main.cpp
@@ -1,8 +1,19 @@
 #include <cstdlib>
 #include <iostream>
 
+#include <vector>
+#include <gasp/tokenizer.hpp>
+
 int main(int argc, char *argv[])
 {
    std::cout << "GASP - by Stefano Anelli." << std::endl;
+
+   gasp::common::tokenizer<int> tokenizer;
+   tokenizer.add(1, "program", false);
+   tokenizer.add(2, "[a-zA-Z_][a-zA-z0-9_]*", true);
+
+   std::vector<gasp::common::token<int>> tokens;
+   tokenizer.parse("program hello;", tokens);
+
    return EXIT_SUCCESS;
 }
