0401590c58a23163c06a0c67b1852ec3d0234ab9
Stefano Anelli <stefano.anelli@gmail.com>
2019-05-23 21:57:56 +0100

    common: added parser context

--- blaise-lang/include/gasp/blaise/parser.hpp
+++ blaise-lang/include/gasp/blaise/parser.hpp
@@ -12,7 +12,7 @@ namespace gasp::blaise
 class blaise_parser : public gasp::common::parser<blaise_token>
 {
    static void parse_program(unsigned int& index, const std::vector<gasp::common::token<gasp::blaise::blaise_token>> &tokens);
-
+   static void parse_variables(unsigned int& index, const std::vector<gasp::common::token<gasp::blaise::blaise_token>> &tokens);
 public:
    void parse(const std::vector<gasp::common::token<gasp::blaise::blaise_token>> &tokens) const;
 };
--- common/include/gasp/common/parser.hpp
+++ common/include/gasp/common/parser.hpp
@@ -21,6 +21,20 @@ public:
 };
 
 template <typename TToken>
+class parser_context
+{
+   std::vector<gasp::common::token<TToken>> _tokens;
+   unsigned int _index;
+
+public:
+   void add_token(TToken token, std::string value, int line, int column) { return _tokens.emplace(token, value, line, column); }
+   gasp::common::token<TToken> token(unsigned int index) { return _tokens.at(index); }
+   gasp::common::token<TToken> peek() { return _tokens.at(_index); }
+   unsigned int index() { return _index; }
+   unsigned int next() { return ++_index; }
+};
+
+template <typename TToken>
 class parser
 {
 protected:
@@ -38,7 +52,7 @@ protected:
          ++index;
          return value;
       }
-      
+
       throw parser_error(tokens.at(index).line(), tokens.at(index).column(), "Unexpected token");
    }
 };
