0e99da6deecaab31ed7b13e51c64670f68fb6dda
Stefano Anelli <stefano.anelli@gmail.com>
2019-05-30 01:53:11 +0100

    gasp-lang: started adding tests for parser/semantic tree
    At present the only simple test fails (actually it gets stuck)

---
+++ blaise-lang/test/parser.cpp
@@ -0,0 +1,26 @@
+#include <catch2/catch.hpp>
+#include <fstream>
+
+#include <gasp/blaise/tokenizer.hpp>
+#include <gasp/blaise/tokens.hpp>
+#include <gasp/blaise/parser.hpp>
+#include <gasp/blaise/language.hpp>
+#include <gasp/common/output.hpp>
+#include <gasp/common/debug.hpp>
+
+using namespace std;
+using namespace gasp::common;
+using namespace gasp::blaise;
+
+SCENARIO("Parse testing programs", "[blaise-lang][parser][base]") {
+
+   blaise_tokenizer tokenizer;
+   blaise_parser parser;
+   blaise_parser_context context;
+
+    GIVEN("simple_empty_program.blaise") {
+       ifstream stream("simple_empty_program.blaise", std::ifstream::in);
+       tokenizer.tokenize(stream, context);
+       parser.parse(context);
+    }
+}
