a67c9164021bd6f80bd577b7a4b92da82e322d38
Stefano Anelli <stefano.anelli@gmail.com>
2019-07-25 22:08:24 +0100

    torricelly-lang: Added a list of basic instruct codes

--- torricelly-lang/include/gasp/torricelly/torricelly.hpp
+++ torricelly-lang/include/gasp/torricelly/torricelly.hpp
@@ -3,3 +3,4 @@
 #include <gasp/torricelly/impl/torricelly_module.hpp>
 #include <gasp/torricelly/impl/torricelly_type.hpp>
 #include <gasp/torricelly/impl/torricelly_subroutine.hpp>
+#include <gasp/torricelly/impl/torricelly_instruction_code.hpp>
