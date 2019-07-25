0393237cb33f9b180fcc89bb89fa36942f2afc8a
Stefano Anelli <stefano.anelli@gmail.com>
2019-07-25 21:18:41 +0100

    torricelly-lang: Implement system type definition

--- torricelly-lang/include/gasp/torricelly/impl/torricelly_type.hpp
+++ torricelly-lang/include/gasp/torricelly/impl/torricelly_type.hpp
@@ -6,7 +6,7 @@ namespace gasp::torricelly {
 
 enum class torricelly_type_type {
    UNDEFINED,
-   BASIC,
+   SYSTEM,
    DERIVED
 };
 
@@ -20,11 +20,11 @@ enum class torricelly_system_type_type {
 };
 
 class torricelly_type {
-   torricelly_type_type _type;
+   torricelly_type_type _type_type;
 protected:
-   torricelly_type();
+   torricelly_type(torricelly_type_type type_type);
 public:
-    ~torricelly_type();
+   virtual ~torricelly_type();
    torricelly_type_type type_type() const;
    inline virtual bool equals(std::shared_ptr<torricelly_type> other) const = 0;
 };
@@ -33,12 +33,13 @@ class toricelly_system_type : public torricelly_type {
    torricelly_system_type_type _system_type;
    toricelly_system_type(torricelly_system_type_type system_type);
    public:
-   ~toricelly_system_type();
+   ~toricelly_system_type() override;
    torricelly_system_type_type system_type() const;
    inline bool equals(std::shared_ptr<torricelly_type> other) const override;
+
+   friend gasp::common::memory;
 };
 
 std::shared_ptr<toricelly_system_type> make_torricelly_system_type(torricelly_system_type_type system_type);
-
-
+   
 }
--- torricelly-lang/src/torricelly_type.cpp
+++ torricelly-lang/src/torricelly_type.cpp
@@ -7,3 +7,20 @@
 using namespace gasp::common;
 using namespace gasp::torricelly;
 
+torricelly_type::torricelly_type(torricelly_type_type type_type) : _type_type(type_type) {}
+torricelly_type::~torricelly_type() {}
+torricelly_type_type torricelly_type::type_type() const { return _type_type; }
+
+toricelly_system_type::toricelly_system_type(torricelly_system_type_type system_type) 
+   : torricelly_type(torricelly_type_type::SYSTEM),
+      _system_type(system_type) {}
+toricelly_system_type::~toricelly_system_type() { }
+torricelly_system_type_type toricelly_system_type::system_type() const { return _system_type; }
+inline bool toricelly_system_type::equals(std::shared_ptr<torricelly_type> other) const { 
+   return other->type_type() == type_type() && 
+          std::dynamic_pointer_cast<toricelly_system_type>(other)->system_type() == system_type();
+}
+
+std::shared_ptr<toricelly_system_type> make_torricelly_system_type(torricelly_system_type_type system_type){
+   return memory::gasp_make_shared<toricelly_system_type>(system_type);
+}
