e77fb487df93ee52a8ef204714d74eb44e64b132
Stefano Anelli <stefano.anelli@gmail.com>
2019-07-27 10:40:06 +0100

    torricelly-lang: subroutine can have instructions added

--- torricelly-lang/include/gasp/torricelly/impl/torricelly_subroutine.hpp
+++ torricelly-lang/include/gasp/torricelly/impl/torricelly_subroutine.hpp
@@ -6,6 +6,7 @@
 
 #include <gasp/common/memory.hpp>
 #include <gasp/torricelly/impl/torricelly_type.hpp>
+#include <gasp/torricelly/impl/torricelly_instruction.hpp>
 
 namespace gasp::torricelly {
 
@@ -14,12 +15,21 @@ class torricelly_subroutine {
    std::shared_ptr<torricelly::torricelly_type> _return_type;
    std::vector<std::shared_ptr<torricelly::torricelly_type>> _parameters;
    std::vector<std::shared_ptr<torricelly::torricelly_type>> _variables;
+   std::vector<std::shared_ptr<torricelly::torricelly_instruction>> _instructions;
+
    torricelly_subroutine(const std::string& name, std::shared_ptr<torricelly::torricelly_type> return_type);
 public:
    std::string name() const;
    std::shared_ptr<torricelly::torricelly_type> return_type() const;
    unsigned int add_parameter(std::shared_ptr<torricelly::torricelly_type> type);
    unsigned int add_variable(std::shared_ptr<torricelly::torricelly_type> type);
+
+   unsigned int append_instruction(std::shared_ptr<torricelly::torricelly_instruction> instruction);
+   void insert_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction);
+   void replace_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction);
+   void remove_instruction(unsigned int index);
+   std::shared_ptr<torricelly::torricelly_instruction> get_instruction(unsigned int index) const;
+   unsigned int count_instructions() const;
 
    friend gasp::common::memory;
 };
--- torricelly-lang/src/torricelly_subroutine.cpp
+++ torricelly-lang/src/torricelly_subroutine.cpp
@@ -23,6 +23,26 @@ unsigned int torricelly_subroutine::add_variable(std::shared_ptr<torricelly::tor
    return _variables.size();
 }
 
+unsigned int torricelly_subroutine::append_instruction(std::shared_ptr<torricelly::torricelly_instruction> instruction) {
+   _instructions.push_back(instruction);
+   return _instructions.size();
+}
+void torricelly_subroutine::insert_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction) {
+   _instructions.insert(_instructions.begin() + index - 1, instruction);
+}
+void torricelly_subroutine::replace_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction) {
+   _instructions.at(index - 1) = instruction;
+}
+void torricelly_subroutine::remove_instruction(unsigned int index) { 
+   _instructions.erase(_instructions.begin() + index - 1); 
+}
+std::shared_ptr<torricelly::torricelly_instruction> torricelly_subroutine::get_instruction(unsigned int index) const { 
+   return _instructions.at(index-1); 
+}
+unsigned int torricelly_subroutine::count_instructions() const { 
+   return _instructions.size(); 
+}
+
 std::shared_ptr<torricelly::torricelly_subroutine> torricelly::make_torricelly_subroutine(const std::string& name, std::shared_ptr<torricelly::torricelly_type> return_type){
    return memory::gasp_make_shared<torricelly_subroutine>(name, return_type);
 }
