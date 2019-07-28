86094d3bf9b774543501b348b2c16ab72256a054
Stefano Anelli <stefano.anelli@gmail.com>
2019-07-28 22:53:19 +0100

    torricelly-lang: improved subroutine code definition

--- torricelly-lang/include/gasp/torricelly/impl/torricelly_subroutine.hpp
+++ torricelly-lang/include/gasp/torricelly/impl/torricelly_subroutine.hpp
@@ -7,30 +7,38 @@
 #include <gasp/common/memory.hpp>
 #include <gasp/torricelly/impl/torricelly_type.hpp>
 #include <gasp/torricelly/impl/torricelly_instruction.hpp>
+#include <gasp/torricelly/impl/torricelly_variable_value.hpp>
 
 namespace gasp::torricelly {
 
 class torricelly_subroutine {
    std::string _name;
    std::shared_ptr<torricelly::torricelly_type> _return_type;
-   std::vector<std::shared_ptr<torricelly::torricelly_type>> _variables;
+   std::vector<std::shared_ptr<torricelly::torricelly_type>> _variable_types;
+   std::vector<torricelly_value> _variable_initial_values;
    std::vector<std::shared_ptr<torricelly::torricelly_instruction>> _instructions;
    unsigned int _labels_counter;
-
+   unsigned int _number_of_parameters;
    torricelly_subroutine(const std::string& name, std::shared_ptr<torricelly::torricelly_type> return_type);
 public:
    std::string name() const;
    std::shared_ptr<torricelly::torricelly_type> return_type() const;
-   unsigned int add_variable(std::shared_ptr<torricelly::torricelly_type> type);
+   unsigned int add_variable(std::shared_ptr<torricelly::torricelly_type> type, torricelly_value initial_value, bool is_parameter = false);
+   std::shared_ptr<torricelly::torricelly_type> get_variable_type(unsigned int index) const;
+   torricelly_value get_initial_value(unsigned int index) const;
+   unsigned int get_number_of_variables() const;
+   unsigned int get_number_of_parameters() const;
 
    unsigned int append_instruction(std::shared_ptr<torricelly::torricelly_instruction> instruction);
    void insert_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction);
    void replace_instruction(unsigned int index, std::shared_ptr<torricelly::torricelly_instruction> instruction);
    void remove_instruction(unsigned int index);
    std::shared_ptr<torricelly::torricelly_instruction> get_instruction(unsigned int index) const;
-   unsigned int count_instructions() const;
+   unsigned int get_number_of_instructions() const;
 
    unsigned int next_label();
+
+   void validate() const;
 
    friend gasp::common::memory;
 };
--- torricelly-lang/src/torricelly_subroutine.cpp
+++ torricelly-lang/src/torricelly_subroutine.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 
+#include <gasp/common/string.hpp>
 #include <gasp/common/memory.hpp>
 #include <gasp/torricelly/torricelly.hpp>
 
@@ -11,13 +12,32 @@ using namespace gasp::torricelly;
 
 torricelly_subroutine::torricelly_subroutine(const std::string& name, 
    std::shared_ptr<torricelly::torricelly_type> return_type)
-   : _name(name), _return_type(return_type) { }
+   : _name(name), _return_type(return_type), _number_of_parameters(0) { }
 std::string torricelly_subroutine::name() const { return _name; };
 std::shared_ptr<torricelly::torricelly_type> torricelly_subroutine::return_type() const { return _return_type; }
 
-unsigned int torricelly_subroutine::add_variable(std::shared_ptr<torricelly::torricelly_type> type) {
-   _variables.push_back(type);
-   return _variables.size();
+unsigned int torricelly_subroutine::add_variable(std::shared_ptr<torricelly::torricelly_type> type, torricelly_value initial_value, bool is_parameter) {
+   _variable_types.push_back(type);
+   _variable_initial_values.push_back(initial_value);
+   if(is_parameter)
+      _number_of_parameters++;
+   return _variable_types.size();
+}
+std::shared_ptr<torricelly::torricelly_type> torricelly_subroutine::get_variable_type(unsigned int index) const {
+   if(index > _variable_types.size())
+      throw torricelly_error(make_string("Cannot access variable type at index ", index,"."));
+   return _variable_types.at(index - 1);
+}
+torricelly_value torricelly_subroutine::get_initial_value(unsigned int index) const {
+   if(index > _variable_initial_values.size())
+      throw torricelly_error(make_string("Cannot access variable initial value at index ", index,"."));
+   return _variable_initial_values.at(index -1);
+}
+unsigned int torricelly_subroutine::get_number_of_variables() const {
+   return _variable_types.size();
+}
+unsigned int torricelly_subroutine::get_number_of_parameters() const {
+   return _number_of_parameters;
 }
 
 unsigned int torricelly_subroutine::append_instruction(std::shared_ptr<torricelly::torricelly_instruction> instruction) {
@@ -36,7 +56,7 @@ void torricelly_subroutine::remove_instruction(unsigned int index) {
 std::shared_ptr<torricelly::torricelly_instruction> torricelly_subroutine::get_instruction(unsigned int index) const { 
    return _instructions.at(index-1); 
 }
-unsigned int torricelly_subroutine::count_instructions() const { 
+unsigned int torricelly_subroutine::get_number_of_instructions() const { 
    return _instructions.size(); 
 }
 
@@ -44,6 +64,16 @@ unsigned int torricelly_subroutine::next_label(){
    return ++_labels_counter;
 }
 
+void torricelly_subroutine::validate() const { 
+   const unsigned int num_of_par = get_number_of_parameters();
+   const unsigned int num_of_var = get_number_of_variables();
+   if(num_of_par > num_of_var)
+      throw torricelly_error(make_string("The number of parameters (", num_of_par,") is greather than the number of variables (",num_of_var,")"));
+   for(auto instruction : _instructions)
+      instruction->validate();
+}
+
 std::shared_ptr<torricelly::torricelly_subroutine> torricelly::make_torricelly_subroutine(const std::string& name, std::shared_ptr<torricelly::torricelly_type> return_type){
    return memory::gasp_make_shared<torricelly_subroutine>(name, return_type);
 }
+
