bafc2627c214de64a1a948adbc522295ddefdc7b
Stefano Anelli <stefano.anelli@gmail.com>
2019-07-28 15:43:23 +0100

    torricelly-lang: inst codes work with output stream

--- torricelly-lang/include/gasp/torricelly/impl/torricelly_instruction_code_io.hpp
+++ torricelly-lang/include/gasp/torricelly/impl/torricelly_instruction_code_io.hpp
@@ -1,10 +1,13 @@
 #pragma once
 
+#include <ostream>
+
 #include <gasp/torricelly/impl/torricelly_io.hpp>
 #include <gasp/torricelly/impl/torricelly_instruction_code.hpp>
 
 namespace gasp::torricelly {
 
 torricelly_text_output& operator<<(torricelly_text_output& os, torricelly_inst_code code);
+std::ostream& operator<<(std::ostream& os, torricelly_inst_code code);
 
 }
--- torricelly-lang/src/torricelly_instruction_code_io.cpp
+++ torricelly-lang/src/torricelly_instruction_code_io.cpp
@@ -6,3 +6,7 @@ using namespace gasp::torricelly;
 torricelly_text_output& torricelly::operator<<(torricelly_text_output& os, torricelly_inst_code code) {
    return os << to_string(code);
 }
+
+std::ostream& torricelly::operator<<(std::ostream& os, torricelly_inst_code code){
+   return os << to_string(code);
+}
