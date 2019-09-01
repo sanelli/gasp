b2697c2d4b79af6369d93b7767b2b550418f0ea1
Stefano Anelli <stefano.anelli@gmail.com>
2019-09-01 12:23:50 +0100

    torricelly: continued implementation of transaltion
    Translation between blaise and torricelly gotta be a bit boring 🙄

--- blaise-to-torricelly/include/gasp/blaise-to-torricelly/impl/translator.hpp
+++ blaise-to-torricelly/include/gasp/blaise-to-torricelly/impl/translator.hpp
@@ -18,6 +18,8 @@ class translator
    std::string get_mangled_module_name(const std::string &name) const;
    std::string get_mangled_subroutine_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_subroutine> subroutine) const;
    std::string get_mangled_type_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_type> type) const;
+
+   std::shared_ptr<gasp::torricelly::torricelly_module> translate_module(std::shared_ptr<blaise::ast::blaise_ast_module> module) const;
 public:
    translator(std::shared_ptr<blaise::ast::blaise_ast_module> blaise_module);
    std::shared_ptr<blaise::ast::blaise_ast_module> blaise_module() const;
--- blaise-to-torricelly/src/translator.cpp
+++ blaise-to-torricelly/src/translator.cpp
@@ -19,8 +19,17 @@ std::shared_ptr<blaise::ast::blaise_ast_module> blaise_to_torricelly::translator
 
 void blaise_to_torricelly::translator::execute(std::vector<std::shared_ptr<gasp::torricelly::torricelly_module>> &torricelly_modules) const
 {
-   if(has_translated(torricelly_modules, blaise_module()->name()))
+   if (has_translated(torricelly_modules, blaise_module()->name()))
       return;
+   
+   auto module = translate_module(blaise_module());
+   torricelly_modules.push_back(module);
+}
+
+std::shared_ptr<gasp::torricelly::torricelly_module> blaise_to_torricelly::translator::translate_module(std::shared_ptr<blaise::ast::blaise_ast_module> blaise_module) const{
+   auto mangled_module_name = get_mangled_module_name(blaise_module->name());
+   auto torricelly_module = make_torricelly_module(mangled_module_name);
+   return torricelly_module;
 }
 
 bool blaise_to_torricelly::translator::has_translated(const std::vector<std::shared_ptr<gasp::torricelly::torricelly_module>> &torricelly_modules,
@@ -28,43 +37,54 @@ bool blaise_to_torricelly::translator::has_translated(const std::vector<std::sha
 {
    auto mangled_name = get_mangled_module_name(module_name);
    auto found = std::find_if(torricelly_modules.begin(), torricelly_modules.end(),
-                [mangled_name](std::shared_ptr<gasp::torricelly::torricelly_module> module) { return module->module_name() == mangled_name; });
+                             [mangled_name](std::shared_ptr<gasp::torricelly::torricelly_module> module) { return module->module_name() == mangled_name; });
    return found != torricelly_modules.end();
 }
 
-std::string blaise_to_torricelly::translator::get_mangled_module_name(const std::string &name) const {
+std::string blaise_to_torricelly::translator::get_mangled_module_name(const std::string &name) const
+{
    return name;
 }
-std::string blaise_to_torricelly::translator::get_mangled_subroutine_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_subroutine> subroutine) const {
-  
-  auto arity = subroutine->get_arity();
-  std::string signature;
-  for(auto parameter_index = 0; parameter_index < arity; ++parameter_index) {
-     auto type = subroutine->get_parameter_type(parameter_index);
-     auto mangled_type_name = get_mangled_type_name(type);
-     signature += mangled_type_name;
-  }
-  auto module_mangled_name = get_mangled_module_name(subroutine->module().lock()->name());
-  return sanelli::make_string(module_mangled_name,".", subroutine->name(), "_", signature);
+std::string blaise_to_torricelly::translator::get_mangled_subroutine_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_subroutine> subroutine) const
+{
+
+   auto arity = subroutine->get_arity();
+   std::string signature;
+   for (auto parameter_index = 0; parameter_index < arity; ++parameter_index)
+   {
+      auto type = subroutine->get_parameter_type(parameter_index);
+      auto mangled_type_name = get_mangled_type_name(type);
+      signature += mangled_type_name;
+   }
+   auto module_mangled_name = get_mangled_module_name(subroutine->module().lock()->name());
+   return sanelli::make_string(module_mangled_name, ".", subroutine->name(), "_", signature);
 }
 
-std::string blaise_to_torricelly::translator::get_mangled_type_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_type> type) const {
-   switch(type->type_type()){
-      case blaise::ast::blaise_ast_type_type::PLAIN:
-         {
-            auto plain_type = blaise::ast::blaise_ast_utility::as_plain_type(type);
-            switch(plain_type->system_type()) {
-               case blaise::ast::blaise_ast_system_type::BOOLEAN: return "b";
-               case blaise::ast::blaise_ast_system_type::CHAR: return "c";
-               case blaise::ast::blaise_ast_system_type::VOID : return "v";
-               case blaise::ast::blaise_ast_system_type::FLOAT: return "f";
-               case blaise::ast::blaise_ast_system_type::INTEGER: return "i";
-               default:
-                  throw gasp::common::gasp_internal_error("Mangling not implemeneted for system type");
-            }
-         }
-         break;
+std::string blaise_to_torricelly::translator::get_mangled_type_name(std::shared_ptr<gasp::blaise::ast::blaise_ast_type> type) const
+{
+   switch (type->type_type())
+   {
+   case blaise::ast::blaise_ast_type_type::PLAIN:
+   {
+      auto plain_type = blaise::ast::blaise_ast_utility::as_plain_type(type);
+      switch (plain_type->system_type())
+      {
+      case blaise::ast::blaise_ast_system_type::BOOLEAN:
+         return "b";
+      case blaise::ast::blaise_ast_system_type::CHAR:
+         return "c";
+      case blaise::ast::blaise_ast_system_type::VOID:
+         return "v";
+      case blaise::ast::blaise_ast_system_type::FLOAT:
+         return "f";
+      case blaise::ast::blaise_ast_system_type::INTEGER:
+         return "i";
       default:
-         throw gasp::common::gasp_internal_error("Mangling not implemeneted for type");
+         throw gasp::common::gasp_internal_error("Mangling not implemeneted for system type");
+      }
+   }
+   break;
+   default:
+      throw gasp::common::gasp_internal_error("Mangling not implemeneted for type");
    }
 }
