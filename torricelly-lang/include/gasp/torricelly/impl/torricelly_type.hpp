98a5740f0acacd2624a7cceefbe2baf68a3456ec
Stefano Anelli <stefano.anelli@gmail.com>
2019-09-04 20:50:25 +0100

    torricelly-lang: added c++ type cast utility

--- torricelly-lang/include/gasp/torricelly/impl/torricelly_type.hpp
+++ torricelly-lang/include/gasp/torricelly/impl/torricelly_type.hpp
@@ -6,15 +6,18 @@
 
 #include <sanelli/sanelli.hpp>
 
-namespace gasp::torricelly {
+namespace gasp::torricelly
+{
 
-enum class torricelly_type_type : unsigned char {
+enum class torricelly_type_type : unsigned char
+{
    UNDEFINED,
    SYSTEM,
    STRUCTURED
 };
 
-enum class torricelly_system_type_type : unsigned char {
+enum class torricelly_system_type_type : unsigned char
+{
    UNDEFINED,
    VOID,
    INTEGER,
@@ -25,25 +28,36 @@ enum class torricelly_system_type_type : unsigned char {
    STRING_LITERAL // Used to define literal constants in the code (for instance for calling functions)
 };
 
-class torricelly_type {
+class torricelly_type
+{
    torricelly_type_type _type_type;
+
 protected:
    torricelly_type(torricelly_type_type type_type);
+
 public:
    virtual ~torricelly_type();
    torricelly_type_type type_type() const;
    inline virtual bool equals(std::shared_ptr<torricelly_type> other) const = 0;
 };
 
-class toricelly_system_type : public torricelly_type {
+class toricelly_system_type : public torricelly_type
+{
    torricelly_system_type_type _system_type;
    toricelly_system_type(torricelly_system_type_type system_type);
-   public:
+
+public:
    ~toricelly_system_type() override;
    torricelly_system_type_type system_type() const;
    inline bool equals(std::shared_ptr<torricelly_type> other) const override;
 
    friend sanelli::memory;
+};
+
+class torricelly_type_utility
+{
+public:
+   static std::shared_ptr<toricelly_system_type> as_system_type(std::shared_ptr<torricelly_type> type);
 };
 
 std::shared_ptr<toricelly_system_type> make_torricelly_system_type(torricelly_system_type_type system_type);
@@ -56,4 +70,4 @@ std::string to_string(torricelly_type_type type);
 std::string to_string(torricelly_system_type_type type);
 std::string to_string(const std::shared_ptr<const torricelly_type> type);
 
-}
+} // namespace gasp::torricelly
--- torricelly-lang/src/torricelly_type.cpp
+++ torricelly-lang/src/torricelly_type.cpp
@@ -12,46 +12,60 @@ torricelly_type::torricelly_type(torricelly_type_type type_type) : _type_type(ty
 torricelly_type::~torricelly_type() {}
 torricelly_type_type torricelly_type::type_type() const { return _type_type; }
 
-toricelly_system_type::toricelly_system_type(torricelly_system_type_type system_type) 
-   : torricelly_type(torricelly_type_type::SYSTEM),
+toricelly_system_type::toricelly_system_type(torricelly_system_type_type system_type)
+    : torricelly_type(torricelly_type_type::SYSTEM),
       _system_type(system_type) {}
-toricelly_system_type::~toricelly_system_type() { }
+toricelly_system_type::~toricelly_system_type() {}
 torricelly_system_type_type toricelly_system_type::system_type() const { return _system_type; }
-inline bool toricelly_system_type::equals(std::shared_ptr<torricelly_type> other) const { 
-   return other->type_type() == type_type() && 
+inline bool toricelly_system_type::equals(std::shared_ptr<torricelly_type> other) const
+{
+   return other->type_type() == type_type() &&
           std::dynamic_pointer_cast<toricelly_system_type>(other)->system_type() == system_type();
 }
 
-std::shared_ptr<toricelly_system_type> torricelly::make_torricelly_system_type(torricelly_system_type_type system_type){
+std::shared_ptr<toricelly_system_type> torricelly_type_utility::as_system_type(std::shared_ptr<torricelly_type> type)
+{
+   return std::static_pointer_cast<toricelly_system_type>(type);
+}
+
+std::shared_ptr<toricelly_system_type> torricelly::make_torricelly_system_type(torricelly_system_type_type system_type)
+{
    return memory::make_shared<toricelly_system_type>(system_type);
 }
 
-std::ostream &torricelly::operator<<(std::ostream &os, torricelly_type_type type){
+std::ostream &torricelly::operator<<(std::ostream &os, torricelly_type_type type)
+{
    return os << to_string(type);
 }
 
-std::ostream &torricelly::operator<<(std::ostream &os, torricelly_system_type_type type) {
+std::ostream &torricelly::operator<<(std::ostream &os, torricelly_system_type_type type)
+{
    return os << to_string(type);
 }
 
-std::ostream &torricelly::operator<<(std::ostream &os, const std::shared_ptr<const torricelly_type> type) {
+std::ostream &torricelly::operator<<(std::ostream &os, const std::shared_ptr<const torricelly_type> type)
+{
    return os << to_string(type);
 }
 
-std::string torricelly::to_string(torricelly_type_type type) {
-   switch(type){
-      case torricelly_type_type::UNDEFINED:
-         return "undefined";
-      case torricelly_type_type::SYSTEM:
-         return "system";
-      case torricelly_type_type::STRUCTURED:
-         return "structured";
-      default:
-         throw torricelly_error("Unknown torricelly type");
+std::string torricelly::to_string(torricelly_type_type type)
+{
+   switch (type)
+   {
+   case torricelly_type_type::UNDEFINED:
+      return "undefined";
+   case torricelly_type_type::SYSTEM:
+      return "system";
+   case torricelly_type_type::STRUCTURED:
+      return "structured";
+   default:
+      throw torricelly_error("Unknown torricelly type");
    }
 }
-std::string torricelly::to_string(torricelly_system_type_type type){
-   switch(type){
+std::string torricelly::to_string(torricelly_system_type_type type)
+{
+   switch (type)
+   {
    case torricelly_system_type_type::UNDEFINED:
       return "undefined";
    case torricelly_system_type_type::VOID:
@@ -70,18 +84,20 @@ std::string torricelly::to_string(torricelly_system_type_type type){
       throw torricelly_error("Unknown torricelly system type");
    }
 }
-std::string torricelly::to_string(const std::shared_ptr<const torricelly_type> type) { 
-   switch(type->type_type()){
-      case torricelly_type_type::UNDEFINED:
-         return to_string(torricelly_system_type_type::UNDEFINED);
-      case torricelly_type_type::SYSTEM:
-      {
-         auto system_type = std::dynamic_pointer_cast<const toricelly_system_type>(type);
-         return to_string(torricelly_type_type::SYSTEM) + "::" + to_string(system_type->system_type());
-      }
-      case torricelly_type_type::STRUCTURED:
-         return to_string(torricelly_type_type::STRUCTURED);
-      default:
-         throw torricelly_error("Unknown type");
+std::string torricelly::to_string(const std::shared_ptr<const torricelly_type> type)
+{
+   switch (type->type_type())
+   {
+   case torricelly_type_type::UNDEFINED:
+      return to_string(torricelly_system_type_type::UNDEFINED);
+   case torricelly_type_type::SYSTEM:
+   {
+      auto system_type = std::dynamic_pointer_cast<const toricelly_system_type>(type);
+      return to_string(torricelly_type_type::SYSTEM) + "::" + to_string(system_type->system_type());
+   }
+   case torricelly_type_type::STRUCTURED:
+      return to_string(torricelly_type_type::STRUCTURED);
+   default:
+      throw torricelly_error("Unknown type");
    }
 }
