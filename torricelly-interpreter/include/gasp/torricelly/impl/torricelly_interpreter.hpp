fc9dc48591d6c05197db8e4adabbbc8db965aa43
Stefano Anelli <stefano.anelli@gmail.com>
2019-09-14 18:42:36 +0100

    torricelly-interpreter: started implementing the interpreter

--- torricelly-interpreter/include/gasp/torricelly/impl/torricelly_interpreter.hpp
+++ torricelly-interpreter/include/gasp/torricelly/impl/torricelly_interpreter.hpp
@@ -1,16 +1,44 @@
 #pragma once
 
 #include <memory>
+#include <stack>
+#include <string>
+
 #include <gasp/torricelly/torricelly.hpp>
+#include <gasp/torricelly/impl/activation_record.hpp>
+#include <gasp/torricelly/impl/activation_record_variable.hpp>
 
 namespace gasp::torricelly::interpreter
 {
 
+enum class torricelly_interpreter_status : unsigned char {
+   ZERO,
+   INITIALIZED,
+   RUNNING,
+   HALTED,
+   FINISHED
+};
+
+std::string to_string(const torricelly_interpreter_status status);
+
+
 class torricelly_interpreter
 {
-   public:
-   torricelly_interpreter();
-   void execute(std::shared_ptr<gasp::torricelly::torricelly_module> main_module);
+   std::shared_ptr<gasp::torricelly::torricelly_module> _main_module;
+   std::stack<std::shared_ptr<torricelly_activation_record>> _activation_records;
+   torricelly_interpreter_status _status;
+
+public:
+   torricelly_interpreter(std::shared_ptr<gasp::torricelly::torricelly_module> main_module);
+   std::shared_ptr<gasp::torricelly::torricelly_module> main_module() const;
+
+   torricelly_interpreter_status status() const;
+
+   void initialize();
+   void run();
+   void step();
+
+   torricelly_activation_record_variable peek_stack() const;
 };
 
 } // namespace gasp::torricelly::interpreter
--- torricelly-interpreter/src/torricelly_interpreter.cpp
+++ torricelly-interpreter/src/torricelly_interpreter.cpp
@@ -1,4 +1,8 @@
 #include <memory>
+#include <string>
+#include <stack>
+
+#include <sanelli/sanelli.hpp>
 
 #include <gasp/torricelly/torricelly.hpp>
 #include <gasp/torricelly/interpreter.hpp>
@@ -7,5 +11,38 @@ using namespace gasp;
 using namespace gasp::torricelly;
 using namespace gasp::torricelly::interpreter;
 
-torricelly_interpreter::torricelly_interpreter() {}
-void torricelly_interpreter::execute(std::shared_ptr<gasp::torricelly::torricelly_module> main_module) {}
+std::string interpreter::to_string(const torricelly_interpreter_status status)
+{
+   switch (status)
+   {
+   case torricelly_interpreter_status::ZERO:
+      return "zero";
+   case torricelly_interpreter_status::INITIALIZED:
+      return "initialized";
+   case torricelly_interpreter_status::RUNNING:
+      return "running";
+   case torricelly_interpreter_status::HALTED:
+      return "halted";
+   case torricelly_interpreter_status::FINISHED:
+      return "finished";
+   default:
+      throw torricelly_interpreter_error("Unknown or unexpected status. It cannot be converted into string");
+   }
+}
+
+torricelly_interpreter::torricelly_interpreter(std::shared_ptr<gasp::torricelly::torricelly_module> main_module)
+    : _status(torricelly_interpreter_status::ZERO), _main_module(main_module) {}
+std::shared_ptr<gasp::torricelly::torricelly_module> torricelly_interpreter::main_module() const { return _main_module; }
+
+torricelly_interpreter_status torricelly_interpreter::status() const { return _status; }
+
+void torricelly_interpreter::initialize() {}
+void torricelly_interpreter::run() {}
+void torricelly_interpreter::step() {}
+
+torricelly_activation_record_variable torricelly_interpreter::peek_stack() const
+{
+   if (_status != torricelly_interpreter_status::FINISHED && _status != torricelly_interpreter_status::HALTED)
+      throw torricelly_interpreter_error(sanelli::make_string("Cannot get top of the stack in status ", to_string(_status)));
+   return _activation_records.top()->peek();
+}
