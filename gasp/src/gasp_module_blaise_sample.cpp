632fe69841b415e61a7a3712bc0d01a4ebed403d
Stefano Anelli <stefano.anelli@gmail.com>
2019-10-17 20:03:50 +0100

    gasp: samples setup refactoring

--- gasp/src/gasp_module_blaise_sample.cpp
+++ gasp/src/gasp_module_blaise_sample.cpp
@@ -11,9 +11,13 @@ using namespace gasp;
 using namespace gasp::common;
 using namespace gasp::module;
 
-std::map<std::string, std::string> _samples;
-std::map<std::string, std::string> _samples_parameters;
-std::map<std::string, std::string> _samples_return_value;
+struct sample_type { 
+   std::string sample;
+   std::string input;
+   std::string output;
+};
+
+std::map<std::string, sample_type> _samples;
 
 auto sample_empty = R"__(program sample;
 begin
@@ -167,209 +171,57 @@ end.)__";
 
 gasp_module_blaise_sample::gasp_module_blaise_sample()
 {
-   _samples["empty"] = sample_empty;
-   _samples_parameters["empty"] = "";
-   _samples_return_value["empty"] = "0";
-
-   _samples["empty-returning-integer"] = sample_empty_returning_integer;
-   _samples_parameters["empty-returning-integer"] = "";
-   _samples_return_value["empty-returning-integer"] = "0";
-
-   _samples["empty-returning-float"] = sample_empty_returning_float;
-   _samples_parameters["empty-returning-float"] = "";
-   _samples_return_value["empty-returning-float"] = "0.000000";
-
-   _samples["empty-returning-boolean"] = sample_empty_returning_boolean;
-   _samples_parameters["empty-returning-boolean"] = "";
-   _samples_return_value["empty-returning-boolean"] = "false";
-
-   _samples["empty-with-parameters"] = sample_empty_with_parameters;
-   _samples_parameters["empty-with-parameters"] = "7 true";
-   _samples_return_value["empty-with-parameters"] = "0";
-
-   _samples["sum-two-integer-parameters"] = sample_sum_two_integer_parameters;
-   _samples_parameters["sum-two-integer-parameters"] = "3 4";
-   _samples_return_value["sum-two-integer-parameters"] = "7";
-
-   _samples["subtract-two-integer-parameters"] = sample_subtract_two_integer_parameters;
-   _samples_parameters["subtract-two-integer-parameters"] = "3 4";
-   _samples_return_value["subtract-two-integer-parameters"] = "-1";
-
-   _samples["multiply-two-integer-parameters"] = sample_multiply_two_integer_parameters;
-   _samples_parameters["multiply-two-integer-parameters"] = "3 4";
-   _samples_return_value["multiply-two-integer-parameters"] = "12";
-
-   _samples["divide-two-integer-parameters"] = sample_divide_two_integer_parameters;
-   _samples_parameters["divide-two-integer-parameters"] = "15 5";
-   _samples_return_value["divide-two-integer-parameters"] = "3";
-
-   _samples["remainder-two-integer-parameters"] = sample_remainder_two_integer_parameters;
-   _samples_parameters["remainder-two-integer-parameters"] = "6 4";
-   _samples_return_value["remainder-two-integer-parameters"] = "2";
-
-   _samples["compare-two-integer-parameters-eq-1"] = sample_eq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-eq-1"] = "5 5";
-   _samples_return_value["compare-two-integer-parameters-eq-1"] = "true";
-
-   _samples["compare-two-integer-parameters-eq-2"] = sample_eq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-eq-2"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-eq-2"] = "false";
-
-   _samples["compare-two-integer-parameters-eq-3"] = sample_eq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-eq-3"] = "5 4";
-   _samples_return_value["compare-two-integer-parameters-eq-3"] = "false";
-
-   _samples["compare-two-integer-parameters-neq-1"] = sample_neq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-neq-1"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-neq-1"] = "true";
-
-   _samples["compare-two-integer-parameters-neq-2"] = sample_neq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-neq-2"] = "5 5";
-   _samples_return_value["compare-two-integer-parameters-neq-2"] = "false";
-
-   _samples["compare-two-integer-parameters-neq-3"] = sample_neq_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-neq-3"] = "5 4";
-   _samples_return_value["compare-two-integer-parameters-neq-3"] = "true";
-
-   _samples["compare-two-integer-parameters-gt-1"] = sample_gt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gt-1"] = "6 5";
-   _samples_return_value["compare-two-integer-parameters-gt-1"] = "true";
-
-   _samples["compare-two-integer-parameters-gt-2"] = sample_gt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gt-2"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-gt-2"] = "false";
-   
-   _samples["compare-two-integer-parameters-gt-3"] = sample_gt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gt-3"] = "4 4";
-   _samples_return_value["compare-two-integer-parameters-gt-3"] = "false";
-
-   _samples["compare-two-integer-parameters-lt-1"] = sample_lt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lt-1"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-lt-1"] = "true";
-
-   _samples["compare-two-integer-parameters-lt-2"] = sample_lt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lt-2"] = "6 5";
-   _samples_return_value["compare-two-integer-parameters-lt-2"] = "false";
-
-   _samples["compare-two-integer-parameters-lt-3"] = sample_lt_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lt-3"] = "5 5";
-   _samples_return_value["compare-two-integer-parameters-lt-3"] = "false";
-
-   _samples["compare-two-integer-parameters-lte-1"] = sample_lte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lte-1"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-lte-1"] = "true";
-
-   _samples["compare-two-integer-parameters-lte-2"] = sample_lte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lte-2"] = "4 4";
-   _samples_return_value["compare-two-integer-parameters-lte-2"] = "true";
-
-   _samples["compare-two-integer-parameters-lte-3"] = sample_lte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-lte-3"] = "5 4";
-   _samples_return_value["compare-two-integer-parameters-lte-3"] = "false";
-
-   _samples["compare-two-integer-parameters-gte-1"] = sample_gte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gte-1"] = "4 5";
-   _samples_return_value["compare-two-integer-parameters-gte-1"] = "false";
-
-   _samples["compare-two-integer-parameters-gte-2"] = sample_gte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gte-2"] = "4 4";
-   _samples_return_value["compare-two-integer-parameters-gte-2"] = "true";
-
-   _samples["compare-two-integer-parameters-gte-3"] = sample_gte_two_integer_parameters;
-   _samples_parameters["compare-two-integer-parameters-gte-3"] = "5 4";
-   _samples_return_value["compare-two-integer-parameters-gte-3"] = "true";
-
-   _samples["sum-two-float-parameters"] = sample_sum_two_float_parameters;
-   _samples_parameters["sum-two-float-parameters"] = "3.5 4.5";
-   _samples_return_value["sum-two-float-parameters"] = "8.000000";
-
-   _samples["subtract-two-float-parameters"] = sample_subtract_two_float_parameters;
-   _samples_parameters["subtract-two-float-parameters"] = "3.5 4.5";
-   _samples_return_value["subtract-two-float-parameters"] = "-1.000000";
-
-   _samples["multiply-two-float-parameters"] = sample_multiply_two_float_parameters;
-   _samples_parameters["multiply-two-float-parameters"] = "3.5 4.5";
-   _samples_return_value["multiply-two-float-parameters"] = "15.750000";
-
-   _samples["divide-two-float-parameters"] = sample_divide_two_float_parameters;
-   _samples_parameters["divide-two-float-parameters"] = "5.0 2.0";
-   _samples_return_value["divide-two-float-parameters"] = "2.500000";
-
-   _samples["sum-two-double-parameters"] = sample_sum_two_double_parameters;
-   _samples_parameters["sum-two-double-parameters"] = "3.5 4.5";
-   _samples_return_value["sum-two-double-parameters"] = "8.000000";
-
-   _samples["subtract-two-double-parameters"] = sample_subtract_two_double_parameters;
-   _samples_parameters["subtract-two-double-parameters"] = "3.5 4.5";
-   _samples_return_value["subtract-two-double-parameters"] = "-1.000000";
-
-   _samples["multiply-two-double-parameters"] = sample_multiply_two_double_parameters;
-   _samples_parameters["multiply-two-double-parameters"] = "3.5 4.5";
-   _samples_return_value["multiply-two-double-parameters"] = "15.750000";
-
-   _samples["divide-two-double-parameters"] = sample_divide_two_double_parameters;
-   _samples_parameters["divide-two-double-parameters"] = "5.0 2.0";
-   _samples_return_value["divide-two-double-parameters"] = "2.500000";
-
-   _samples["ternary-expression-1"] = sample_ternary_expression;
-   _samples_parameters["ternary-expression-1"] = "true";
-   _samples_return_value["ternary-expression-1"] = "1";
-
-   _samples["ternary-expression-2"] = sample_ternary_expression;
-   _samples_parameters["ternary-expression-2"] = "false";
-   _samples_return_value["ternary-expression-2"] = "2";
-
-   _samples["boolean-and-1"] = sample_and_two_boolean_parameters;
-   _samples_parameters["boolean-and-1"] = "true true";
-   _samples_return_value["boolean-and-1"] = "true";
-
-   _samples["boolean-and-2"] = sample_and_two_boolean_parameters;
-   _samples_parameters["boolean-and-2"] = "true false";
-   _samples_return_value["boolean-and-2"] = "false";
-
-   _samples["boolean-and-3"] = sample_and_two_boolean_parameters;
-   _samples_parameters["boolean-and-3"] = "false true";
-   _samples_return_value["boolean-and-3"] = "false";
-
-   _samples["boolean-and-4"] = sample_and_two_boolean_parameters;
-   _samples_parameters["boolean-and-4"] = "false false";
-   _samples_return_value["boolean-and-4"] = "false";
-
-   _samples["boolean-or-1"] = sample_or_two_boolean_parameters;
-   _samples_parameters["boolean-or-1"] = "true true";
-   _samples_return_value["boolean-or-1"] = "true";
-
-   _samples["boolean-or-2"] = sample_or_two_boolean_parameters;
-   _samples_parameters["boolean-or-2"] = "true false";
-   _samples_return_value["boolean-or-2"] = "true";
-
-   _samples["boolean-or-3"] = sample_or_two_boolean_parameters;
-   _samples_parameters["boolean-or-3"] = "false true";
-   _samples_return_value["boolean-or-3"] = "true";
-
-   _samples["boolean-or-4"] = sample_or_two_boolean_parameters;
-   _samples_parameters["boolean-or-4"] = "false false";
-   _samples_return_value["boolean-or-4"] = "false";
-
-   _samples["boolean-not-1"] = sample_not_boolean_parameter;
-   _samples_parameters["boolean-not-1"] = "false";
-   _samples_return_value["boolean-not-1"] = "true";
-
-   _samples["boolean-not-2"] = sample_not_boolean_parameter;
-   _samples_parameters["boolean-not-2"] = "true";
-   _samples_return_value["boolean-not-2"] = "false";
-
-   _samples["integer-binary-literal"] = sample_binary_integer;
-   _samples_parameters["integer-binary-literal"] = "";
-   _samples_return_value["integer-binary-literal"] = "3";
-
-   _samples["integer-octal-literal"] = sample_octal_integer;
-   _samples_parameters["integer-octal-literal"] = "";
-   _samples_return_value["integer-octal-literal"] = "63";
-
-   _samples["integer-hex-literal"] = sample_hexadecimal_integer;
-   _samples_parameters["integer-hex-literal"] = "";
-   _samples_return_value["integer-hex-literal"] = "255";
+   _samples["empty"] = {sample_empty, "", "0"} ;
+   _samples["empty-returning-integer"] = { sample_empty_returning_integer, "", "0" };
+   _samples["empty-returning-float"] = { sample_empty_returning_float, "",  "0.000000" };
+   _samples["empty-returning-boolean"] = { sample_empty_returning_boolean, "", "false" };
+   _samples["empty-with-parameters"] = { sample_empty_with_parameters, "7 true", "0" };
+   _samples["sum-two-integer-parameters"] = { sample_sum_two_integer_parameters, "3 4", "7"};
+   _samples["subtract-two-integer-parameters"] = { sample_subtract_two_integer_parameters, "3 4", "-1" };
+   _samples["multiply-two-integer-parameters"] = { sample_multiply_two_integer_parameters, "3 4", "12" };
+   _samples["divide-two-integer-parameters"] = { sample_divide_two_integer_parameters, "15 5", "3" };
+   _samples["remainder-two-integer-parameters"] = { sample_remainder_two_integer_parameters, "6 4", "2" };
+   _samples["compare-two-integer-parameters-eq-1"] = { sample_eq_two_integer_parameters, "5 5", "true" };
+   _samples["compare-two-integer-parameters-eq-2"] = { sample_eq_two_integer_parameters, "4 5", "false" };
+   _samples["compare-two-integer-parameters-eq-3"] = { sample_eq_two_integer_parameters, "5 4", "false" };
+   _samples["compare-two-integer-parameters-neq-1"] = { sample_neq_two_integer_parameters, "4 5", "true" };
+   _samples["compare-two-integer-parameters-neq-2"] = { sample_neq_two_integer_parameters, "5 5", "false" };
+   _samples["compare-two-integer-parameters-neq-3"] = { sample_neq_two_integer_parameters, "5 4", "true" };
+   _samples["compare-two-integer-parameters-gt-1"] = { sample_gt_two_integer_parameters, "6 5", "true" };
+   _samples["compare-two-integer-parameters-gt-2"] = { sample_gt_two_integer_parameters, "4 5", "false" };
+   _samples["compare-two-integer-parameters-gt-3"] = { sample_gt_two_integer_parameters, "4 4", "false" };
+   _samples["compare-two-integer-parameters-lt-1"] = { sample_lt_two_integer_parameters, "4 5", "true" };
+   _samples["compare-two-integer-parameters-lt-2"] = { sample_lt_two_integer_parameters, "6 5", "false" };
+   _samples["compare-two-integer-parameters-lt-3"] = { sample_lt_two_integer_parameters, "5 5", "false" };
+   _samples["compare-two-integer-parameters-lte-1"] = { sample_lte_two_integer_parameters, "4 5", "true" };
+   _samples["compare-two-integer-parameters-lte-2"] = { sample_lte_two_integer_parameters, "4 4", "true" };
+   _samples["compare-two-integer-parameters-lte-3"] =  {sample_lte_two_integer_parameters, "5 4", "false" };
+   _samples["compare-two-integer-parameters-gte-1"] = { sample_gte_two_integer_parameters, "4 5", "false" };
+   _samples["compare-two-integer-parameters-gte-2"] = { sample_gte_two_integer_parameters, "4 4", "true" };
+   _samples["compare-two-integer-parameters-gte-3"] = { sample_gte_two_integer_parameters, "5 4", "true" };
+   _samples["sum-two-float-parameters"] = { sample_sum_two_float_parameters, "3.5 4.5", "8.000000" };
+   _samples["subtract-two-float-parameters"] = { sample_subtract_two_float_parameters, "3.5 4.5", "-1.000000" };
+   _samples["multiply-two-float-parameters"] = { sample_multiply_two_float_parameters, "3.5 4.5", "15.750000" };
+   _samples["divide-two-float-parameters"] = { sample_divide_two_float_parameters, "5.0 2.0", "2.500000" };
+   _samples["sum-two-double-parameters"] = { sample_sum_two_double_parameters, "3.5 4.5", "8.000000" };
+   _samples["subtract-two-double-parameters"] = { sample_subtract_two_double_parameters, "3.5 4.5", "-1.000000" };
+   _samples["multiply-two-double-parameters"] = { sample_multiply_two_double_parameters, "3.5 4.5", "15.750000" };
+   _samples["divide-two-double-parameters"] = { sample_divide_two_double_parameters, "5.0 2.0", "2.500000" };
+   _samples["ternary-expression-1"] = { sample_ternary_expression, "true", "1" };
+   _samples["ternary-expression-2"] = { sample_ternary_expression, "false", "2" };
+   _samples["boolean-and-1"] = { sample_and_two_boolean_parameters, "true true", "true" };
+   _samples["boolean-and-2"] = { sample_and_two_boolean_parameters, "true false", "false" };
+   _samples["boolean-and-3"] = { sample_and_two_boolean_parameters, "false true", "false" };
+   _samples["boolean-and-4"] = { sample_and_two_boolean_parameters, "false false", "false" };
+   _samples["boolean-or-1"] = { sample_or_two_boolean_parameters, "true true", "true" };
+   _samples["boolean-or-2"] = { sample_or_two_boolean_parameters, "true false", "true" };
+   _samples["boolean-or-3"] = { sample_or_two_boolean_parameters, "false true", "true" };
+   _samples["boolean-or-4"] = { sample_or_two_boolean_parameters, "false false", "false" };
+   _samples["boolean-not-1"] = { sample_not_boolean_parameter, "false", "true" };
+   _samples["boolean-not-2"] = { sample_not_boolean_parameter,  "true", "false" };
+   _samples["integer-binary-literal"] = { sample_binary_integer, "", "3" };
+   _samples["integer-octal-literal"] = { sample_octal_integer, "", "63" };
+   _samples["integer-hex-literal"] = { sample_hexadecimal_integer, "", "255" };
 }
 
 std::string gasp_module_blaise_sample::name() const
@@ -401,22 +253,22 @@ std::string gasp_module_blaise_sample::get_sample(std::string sample) const
 {
    auto it = _samples.find(sample);
    if (it != _samples.end())
-      return it->second;
+      return it->second.sample;
    throw std::runtime_error(sanelli::make_string("Cannot find sample ", sample, "."));
 }
 
 std::string gasp_module_blaise_sample::get_input(std::string sample) const
 {
-   auto it = _samples_parameters.find(sample);
-   if (it != _samples_parameters.end())
-      return it->second;
+   auto it = _samples.find(sample);
+   if (it != _samples.end())
+      return it->second.input;
    throw std::runtime_error(sanelli::make_string("Cannot find sample ", sample, "."));
 }
 std::string gasp_module_blaise_sample::get_output(std::string sample) const
 {
-   auto it = _samples_return_value.find(sample);
-   if (it != _samples_return_value.end())
-      return it->second;
+   auto it = _samples.find(sample);
+   if (it != _samples.end())
+      return it->second.output;
    throw std::runtime_error(sanelli::make_string("Cannot find sample ", sample, "."));
 }
 
@@ -461,10 +313,10 @@ bool gasp_module_blaise_sample::run(int argc, char *argv[])
          return false;
       }
       arg = argv[3];
-      auto it = _samples_parameters.find(arg);
-      if (it != _samples_parameters.end())
+      auto it = _samples.find(arg);
+      if (it != _samples.end())
       {
-         std::cout << it->second << std::endl;
+         std::cout << it->second.input << std::endl;
          return true;
       }
       else
@@ -482,10 +334,10 @@ bool gasp_module_blaise_sample::run(int argc, char *argv[])
          return false;
       }
       arg = argv[3];
-      auto it = _samples_return_value.find(arg);
-      if (it != _samples_return_value.end())
+      auto it = _samples.find(arg);
+      if (it != _samples.end())
       {
-         std::cout << it->second << std::endl;
+         std::cout << it->second.output << std::endl;
          return true;
       }
       else
@@ -499,7 +351,7 @@ bool gasp_module_blaise_sample::run(int argc, char *argv[])
       auto it = _samples.find(arg);
       if (it != _samples.end())
       {
-         std::cout << it->second << std::endl;
+         std::cout << it->second.sample << std::endl;
          return true;
       }
       else
