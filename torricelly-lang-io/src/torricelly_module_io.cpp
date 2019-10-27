fb8f210ab936962bfbc85f6561955dcf0ccd1a22
Stefano Anelli <stefano.anelli@gmail.com>
2019-10-27 10:58:10 +0000

    torricelly-lang-io: torricelly IO can store list of dependencies
    This works for both text and binary

--- torricelly-lang-io/src/torricelly_module_io.cpp
+++ torricelly-lang-io/src/torricelly_module_io.cpp
@@ -8,9 +8,12 @@ using namespace gasp::torricelly;
 
 torricelly_text_output &torricelly::operator<<(torricelly_text_output &os, const std::shared_ptr<const torricelly_module> module)
 {
-
-   os << ".module " << module->module_name() << std::endl;
+   os << ".torricelly " << std::endl; 
    os << ".version " << os.version_major() << "." << os.version_minor() << "." << os.version_build() << std::endl;
+   os << ".module " << module->module_name() << std::endl;
+   os << ".dependencies " << module->count_dependencies() << std::endl;
+   for(auto dependency_index = 0; dependency_index < module->count_dependencies(); ++dependency_index)
+      os << "   .dependency " << module->get_dependency(dependency_index)->module_name() << std::endl;
    os << ".locals " << module->count_locals() << std::endl;
    for (auto variable_index = 1u; variable_index <= module->count_locals(); ++variable_index)
    {
@@ -31,6 +34,9 @@ torricelly_binary_output &torricelly::operator<<(torricelly_binary_output &os, c
 {
    os << 'T' <<'O'<<'R'<<'R'<<'I'<<'C'<<'E'<<'L'<<'L'<<'Y';
    os << os.version_major() << os.version_minor() << os.version_build();
+   os << (int32_t) module->count_dependencies();
+   for(auto dependency_index = 0; dependency_index < module->count_dependencies(); ++dependency_index)
+      os << module->get_dependency(dependency_index)->module_name();
    os << module->module_name();
    os << (int32_t)module->count_locals();
    for (auto variable_index = 1u; variable_index <= module->count_locals(); ++variable_index)
