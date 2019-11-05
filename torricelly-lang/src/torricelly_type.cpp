4f44f0734945d999a6dfd6620eee58a439d1c353
Stefano Anelli <stefano.anelli@gmail.com>
2019-11-05 21:04:20 +0000

    torricelly-lang: fixed is_void function

--- torricelly-lang/src/torricelly_type.cpp
+++ torricelly-lang/src/torricelly_type.cpp
@@ -22,31 +22,40 @@ torricelly_system_type::~torricelly_system_type() {}
 torricelly_system_type_type torricelly_system_type::system_type() const { return _system_type; }
 inline bool torricelly_system_type::equals(std::shared_ptr<torricelly_type> other) const
 {
-   if(other->type_type() != type_type()) return false;
+   if (other->type_type() != type_type())
+      return false;
    auto other_system_type = torricelly_type_utility::as_system_type(other);
    return system_type() == other_system_type->system_type();
 }
 
-torricelly_array_type::torricelly_array_type(std::shared_ptr<torricelly_type> underlying_type, const std::vector<unsigned int>& dimensions)
-: torricelly_type(torricelly_type_type::ARRAY), _underlying_type(underlying_type) {
+torricelly_array_type::torricelly_array_type(std::shared_ptr<torricelly_type> underlying_type, const std::vector<unsigned int> &dimensions)
+    : torricelly_type(torricelly_type_type::ARRAY), _underlying_type(underlying_type)
+{
    std::copy(dimensions.begin(), dimensions.end(), std::back_inserter(_dimensions));
 }
 
-torricelly_array_type::~torricelly_array_type() { }
+torricelly_array_type::~torricelly_array_type() {}
 std::shared_ptr<torricelly_type> torricelly_array_type::underlying_type() const { return _underlying_type; }
 unsigned int torricelly_array_type::dimensions() const { return _dimensions.size(); }
 unsigned int torricelly_array_type::dimension(unsigned int dim) const { return _dimensions.at(dim); }
-inline bool torricelly_array_type::equals(std::shared_ptr<torricelly_type> other) const {
-   if(other->type_type() != type_type()) return false;
+inline bool torricelly_array_type::equals(std::shared_ptr<torricelly_type> other) const
+{
+   if (other->type_type() != type_type())
+      return false;
    auto other_array_type = torricelly_type_utility::as_array_type(other);
-   if(!underlying_type()->equals(other_array_type->underlying_type())) return false;
-   if(dimensions() != other_array_type->dimensions()) return false;
-   for(auto d = 0U; d < dimensions();++d) { 
+   if (!underlying_type()->equals(other_array_type->underlying_type()))
+      return false;
+   if (dimensions() != other_array_type->dimensions())
+      return false;
+   for (auto d = 0U; d < dimensions(); ++d)
+   {
       auto dim = dimension(d);
       auto other_dimension = other_array_type->dimension(d);
-      if(dim == torricelly_array_type::undefined_dimension() || 
-         other_dimension == torricelly_array_type::undefined_dimension()) continue;
-      if( dim != other_dimension) return false;
+      if (dim == torricelly_array_type::undefined_dimension() ||
+          other_dimension == torricelly_array_type::undefined_dimension())
+         continue;
+      if (dim != other_dimension)
+         return false;
    }
    return true;
 }
@@ -62,18 +71,22 @@ std::shared_ptr<torricelly_array_type> torricelly_type_utility::as_array_type(st
    return std::static_pointer_cast<torricelly_array_type>(type);
 }
 
-bool torricelly_type_utility::is_system_type(std::shared_ptr<torricelly_type> type) {
+bool torricelly_type_utility::is_system_type(std::shared_ptr<torricelly_type> type)
+{
    return type->type_type() == torricelly_type_type::SYSTEM;
 }
 
-bool torricelly_type_utility::is_void(std::shared_ptr<torricelly_type> type) { 
-   if(torricelly_type_utility::is_system_type(type)) return false;
+bool torricelly_type_utility::is_void(std::shared_ptr<torricelly_type> type)
+{
+   if (!torricelly_type_utility::is_system_type(type)) return false;
    auto system_type = torricelly_type_utility::as_system_type(type);
    return system_type->system_type() == torricelly_system_type_type::VOID;
 }
 
-bool torricelly_type_utility::is_string_literal(std::shared_ptr<torricelly_type> type) { 
-   if(torricelly_type_utility::is_system_type(type)) return false;
+bool torricelly_type_utility::is_string_literal(std::shared_ptr<torricelly_type> type)
+{
+   if (torricelly_type_utility::is_system_type(type))
+      return false;
    auto system_type = torricelly_type_utility::as_system_type(type);
    return system_type->system_type() == torricelly_system_type_type::STRING_LITERAL;
 }
@@ -83,7 +96,7 @@ std::shared_ptr<torricelly_system_type> torricelly::make_torricelly_system_type(
    return memory::make_shared<torricelly_system_type>(system_type);
 }
 
-std::shared_ptr<torricelly_array_type> torricelly::make_torricelly_array_type(std::shared_ptr<torricelly_type> underlying_type, const std::vector<unsigned int>& dimensions)
+std::shared_ptr<torricelly_array_type> torricelly::make_torricelly_array_type(std::shared_ptr<torricelly_type> underlying_type, const std::vector<unsigned int> &dimensions)
 {
    return memory::make_shared<torricelly_array_type>(underlying_type, dimensions);
 }
@@ -157,15 +170,15 @@ std::string torricelly::to_string(const std::shared_ptr<torricelly_type> type)
    case torricelly_type_type::ARRAY:
    {
       auto array_type = torricelly_type_utility::as_array_type(type);
-      auto representation = to_string(torricelly_type_type::ARRAY) + "<" + to_string(array_type->underlying_type())  + ">[";
-      for(auto dimension = 0U; dimension < array_type->dimensions(); ++dimension)
-      {   
-         if(dimension > 0)
+      auto representation = to_string(torricelly_type_type::ARRAY) + "<" + to_string(array_type->underlying_type()) + ">[";
+      for (auto dimension = 0U; dimension < array_type->dimensions(); ++dimension)
+      {
+         if (dimension > 0)
             representation += ", ";
          auto d = array_type->dimension(dimension);
-         representation += d != torricelly_array_type::undefined_dimension() 
-            ? std::to_string(d)
-            : "...";
+         representation += d != torricelly_array_type::undefined_dimension()
+                               ? std::to_string(d)
+                               : "...";
       }
       representation += "]";
       return representation;
