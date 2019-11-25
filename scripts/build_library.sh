7171d4be0948bcfcd728c37fdbd73a8aff6ea8a9
Stefano Anelli <stefano.anelli@gmail.com>
2019-11-25 01:39:43 +0000

    gasp: Initial script for compiling the native library

---
+++ scripts/build_library.sh
@@ -0,0 +1,5 @@
+../build/gasp/gasp compile -i ../build/gasp/library/arrays.mblaise -of torricelly-binary > ../build/gasp/library/arrays.tb
+../build/gasp/gasp compile -i ../build/gasp/library/math.mblaise -of torricelly-binary > ../build/gasp/library/math.tb
+../build/gasp/gasp compile -i ../build/gasp/library/io.mblaise -of torricelly-binary > ../build/gasp/library/io.tb
+../build/gasp/gasp compile -i ../build/gasp/library/random.mblaise -of torricelly-binary > ../build/gasp/library/random.tb
+
