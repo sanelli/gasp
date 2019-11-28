eaab04af4c59f8dbe98f7f9fb79908c1f1d6b491
Stefano Anelli <stefano.anelli@gmail.com>
2019-11-28 21:39:28 +0000

    gasp: Updated script for compiling libraries

--- scripts/build_library.sh
+++ scripts/build_library.sh
@@ -2,4 +2,4 @@
 ../build/gasp/gasp compile -i ../build/gasp/library/math.mblaise -of torricelly-binary > ../build/gasp/library/math.tb
 ../build/gasp/gasp compile -i ../build/gasp/library/io.mblaise -of torricelly-binary > ../build/gasp/library/io.tb
 ../build/gasp/gasp compile -i ../build/gasp/library/random.mblaise -of torricelly-binary > ../build/gasp/library/random.tb
-
+../build/gasp/gasp compile -i ../build/gasp/library/matrices.mblaise -of torricelly-binary > ../build/gasp/library/matrices.tb
