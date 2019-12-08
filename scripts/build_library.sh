f4630aa4c9e9810111f86d5d0b0874aaae5335ae
Stefano Anelli <stefano.anelli@gmail.com>
2019-12-08 19:11:54 +0000

    gasp: library building scripts does not require the list of scripts anymore

--- scripts/build_library.sh
+++ scripts/build_library.sh
@@ -1,6 +1,6 @@
-../build/gasp/gasp compile -i ../build/gasp/library/arrays.mblaise -of torricelly-binary > ../build/gasp/library/arrays.tb
-../build/gasp/gasp compile -i ../build/gasp/library/math.mblaise -of torricelly-binary > ../build/gasp/library/math.tb
-../build/gasp/gasp compile -i ../build/gasp/library/io.mblaise -of torricelly-binary > ../build/gasp/library/io.tb
-../build/gasp/gasp compile -i ../build/gasp/library/random.mblaise -of torricelly-binary > ../build/gasp/library/random.tb
-../build/gasp/gasp compile -i ../build/gasp/library/matrices.mblaise -of torricelly-binary > ../build/gasp/library/matrices.tb
-../build/gasp/gasp compile -i ../build/gasp/library/strings.mblaise -of torricelly-binary > ../build/gasp/library/strings.tb
+for f in ../build/gasp/library/*.mblaise
+do
+  tgt=${f%.mblaise}.tb
+  echo "Compiling '$f' => '$tgt'"
+  ../build/gasp/gasp compile -i $f -of torricelly-binary > $tgt
+done
